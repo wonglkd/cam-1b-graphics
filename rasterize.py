7887ee63927e120b871c056dca082577ff29bcc1
Daniel Wong <wonglkd@gmail.com>
2015-02-21 17:29:51 +0000

    Got rotation semi-working (need to debug), wireframe

--- rasterize.py
+++ rasterize.py
@@ -10,24 +10,83 @@ import obj_file
 import screen
 import triangle
 import numpy as np
+import math
 from pprint import pprint
 
 
-def rasterize_triangle(face):
+def rasterize_triangle(face, m_matrix=None):
     vertices = [v for v, _ in face]
     # perspective
     # strip z value
-    vertices = [v[:2] for v in vertices]
+    if m_matrix is not None:
+        # vertices = map()
+        vertices = map(lambda v: m_matrix.dot(np.append(v, [0])), vertices)
+    # else:
+        vertices = [v[:2] for v in vertices]
     vertices = map(screen.rescale_point, vertices)
     triangle.draw(vertices)
 
 
+def gen_m_matrix(camera_pos, look_point, up_vector, d):
+    camera_look_dist = np.linalg.norm(camera_pos - look_point)
+    d_over_el = d / camera_look_dist
+    translation_matrix = np.array([(1, 0, 0, -camera_pos[0, 0]),
+                                   (0, 1, 0, -camera_pos[0, 1]),
+                                   (0, 0, 1, -camera_pos[0, 2]),
+                                   (0, 0, 0, 1)])
+    scale_matrix = np.array([(d_over_el, 0, 0, 0),
+                             (0, d_over_el, 0, 0),
+                             (0, 0, d_over_el, 0),
+                             (0, 0, 0,         1)])
+    look_point_transformed = scale_matrix.dot(translation_matrix).dot(look_point.T)
+    theta = math.acos(look_point_transformed[2, 0] / np.sqrt(look_point_transformed[0, 0] ** 2 + look_point_transformed[2, 0] ** 2))
+    theta_cos = math.cos(theta)
+    theta_sin = math.sin(theta)
+    R_1 = np.array([(theta_cos, 0, -theta_sin, 0),
+                    (0, 1, 0, 0),
+                    (theta_sin, 0, theta_cos, 0),
+                    (0, 0, 0, 1)])
+    l_prime_3 = R_1.dot(look_point_transformed)
+    phi = math.acos(l_prime_3[2, 0] / np.sqrt(l_prime_3[1, 0] ** 2 + l_prime_3[2, 0] ** 2))
+    phi_sin = math.sin(phi)
+    phi_cos = math.cos(phi)
+    R_2 = np.array([(1, 0, 0, 0),
+                    (0, phi_cos, -phi_sin, 0),
+                    (0, phi_sin, phi_cos, 0),
+                    (0, 0, 0, 1)])
+    u_prime_3 = R_2.dot(R_1).dot(up_vector.T)
+    psi = math.acos(u_prime_3[1, 0] / np.sqrt(u_prime_3[0, 0] ** 2 + u_prime_3[1, 0] ** 2))
+    psi_cos = math.cos(phi)
+    phi_sin = math.sin(phi)
+    R_3 = np.array([(phi_cos, -phi_sin, 0, 0),
+                    (phi_sin, phi_cos, 0, 0),
+                    (0, 0, 1, 0),
+                    (0, 0, 0, 1)])
+    m_matrix = R_3.dot(R_2).dot(R_1).dot(scale_matrix).dot(translation_matrix)
+    return m_matrix
+
+
 def main():
+    # d = distance from origin to screen centre
+    d = 1.
+    # camera_pos = np.array([[0, 0, 0, 0]])
+    # look_point = np.array([[0, 0, 1., 0]])
+    # up_vector = np.array([[0, 1, 0, 0]])
+
+    # test values
+    camera_pos = np.array([[5, 10, 0, 0]])
+    look_point = np.array([[5, 10, 1, 0]])
+    up_vector = np.array([[5, 15, 1, 0]])
+    m_matrix = gen_m_matrix(camera_pos, look_point, up_vector, d)
+    print m_matrix
+
+
+
     obj = obj_file.load('SV1-utah/wt_teapot.obj')['teapot.005']
     for face in obj.faces:
         try:
             n_face = [(obj.vertices[v], obj.normals[vn]) for v, vn in face]
-            rasterize_triangle(n_face)
+            rasterize_triangle(n_face, m_matrix=m_matrix)
         except IndexError:
             print "cannot find vertex ",
             pprint(face)
@@ -37,9 +96,6 @@ def main():
         # draw_triangle([(50, 50),(50+50,50),(50,50+50)])
         # break
 
-    # T = np.array((1, 0, 0, -e[0]),
-    #              (0, 1, 0, -e[1]),
-    #              (0, 0, 0, 1))
     screen.write('SV1-utah/teapot.png')
 
 
--- triangle.py
+++ triangle.py
@@ -7,8 +7,8 @@ def draw(vertices):
     vs1 = v2[0]-v1[0], v2[1]-v1[1]
     vs2 = v3[0]-v1[0], v3[1]-v1[1]
 
-    for v in vertices:
-        screen.draw_pixel(*v)
+    # for v in vertices:
+    #     screen.draw_pixel(*v)
     for v, u in zip(vertices, vertices[1:] + [vertices[0]]):
         line.draw(v, u)
     return
